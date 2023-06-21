b3000f18d677ca59572b016ed10f114e79c94765
bossboss4169 <109776142+bossboss4169@users.noreply.github.com>
2023-06-21 09:26:31 +0700

    Add files via upload

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            username: { label: "Username", type: "text" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            const res = await fetch("https://www.melivecode.com/api/login", {
+              method: 'POST',
+              body: JSON.stringify(credentials),
+              headers: { "Content-Type": "application/json" }
+            })
+            const data = await res.json()
+
+            if (data.status == 'ok') {
+                return data.user
+            }
+            return null
+          }
+        })
+      ],
+      secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=",
+      callbacks: {
+        async jwt({ token, account }) {
+          if (account) {
+            token.accessToken = account.access_token
+          }
+          return token
+        },
+        async session({ session, token, user }) {
+          session.accessToken = token.accessToken
+          return session
+        }
+      }
+  }
+  export default NextAuth(authOptions)
